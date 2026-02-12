5a65b7874fa6b445e62749d3ce4f4b55aedac372
-xylon <r.smagulov@g.nsu.ru>
2026-02-12 15:36:24 +0700

    2vers

---
+++ 1/README.md
@@ -0,0 +1,11 @@
+# Сборка
+
+1. mkdir build
+2. cd build
+3. cmake .. -DARRAY_TYPE=double или cmake .. -DARRAY_TYPE=float
+4. cmake --build .
+
+# Вывод
+
+Сумма (float): -0.239823
+Сумма (double): 0.000000009011489
--- 1/main.cpp
+++ 1/main.cpp
@@ -1,57 +1,32 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
-#include <chrono>
-#include <cstring>
+#include <iomanip>
+
+#ifdef USE_FLOAT
+    using value_type = float;
+    #define SIN_FUNC(x) sinf(x)
+#else
+    using value_type = double;
+    #define SIN_FUNC(x) sin(x)
+#endif
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        return 1;
+int main() {
+    size_t N = 10000000;
+    vector<value_type> arr(N);
+    value_type sum = 0;
+    for (size_t i = 0; i < N; i++) {
+        value_type arg = static_cast<value_type>(2.0 * M_PI * i / N-1);
+        arr[i] = SIN_FUNC(arg);
+        sum += arr[i];
     }
-    const size_t N = 10000000;
-    const double two_pi = 2.0 * M_PI;
+
+    string type_name = (sizeof(value_type) == 4) ? "float" : "double";
+    int precision = (sizeof(value_type) == 4) ? 6 : 15;
     
-    if (strcmp(argv[1], "float") == 0) {
-        auto start = chrono::high_resolution_clock::now();
-        
-        float* array = new float[N];
-        float sum = 0.0f;
-        
-        for (size_t i = 0; i < N; ++i) {
-            float phase = static_cast<float>(i) / static_cast<float>(N) * static_cast<float>(two_pi);
-            array[i] = sinf(phase); 
-            sum += array[i];
-        }
-        
-        auto end = chrono::high_resolution_clock::now();
-        chrono::duration<double> elapsed = end - start;
-        
-        cout << "Sum: " << sum << endl;
-        cout << "Time: " << elapsed.count() << endl;
-        delete[] array;
+    cout << fixed << setprecision(precision) << "Сумма (" << type_name << "): " << sum << std::endl;
 
-    } else if (strcmp(argv[1], "double") == 0) {
-        auto start = chrono::high_resolution_clock::now();
-
-        double* array = new double[N];
-        double sum = 0.0;
-        
-        for (size_t i = 0; i < N; ++i) {
-            double phase = static_cast<double>(i) / static_cast<double>(N) * two_pi;
-            array[i] = sin(phase);
-            sum += array[i];
-        }
-        
-        auto end = chrono::high_resolution_clock::now();
-        chrono::duration<double> elapsed = end - start;
-        
-        cout << "Sum: " << sum << endl;
-        cout << "Time: " << elapsed.count() << endl;
-        delete[] array;
-
-    } else {
-        return 1;
-    }
     return 0;
 }
