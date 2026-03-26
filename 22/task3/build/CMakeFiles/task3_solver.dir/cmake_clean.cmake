f723a37a6c248757551386b53417f5b18176f64a
Ruslan Smagulov <r.smagulov@gmail.com>
2026-03-26 10:03:37 +0000

    upd

---
+++ 22/task3/build/CMakeFiles/task3_solver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task3_solver.dir/src/task3_simple_iteration.cpp.o"
+  "CMakeFiles/task3_solver.dir/src/task3_simple_iteration.cpp.o.d"
+  "task3_solver"
+  "task3_solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task3_solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
