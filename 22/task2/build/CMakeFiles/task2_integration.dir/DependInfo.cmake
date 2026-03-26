628fe09d5ef6483a4df157a16efabc5b98f5be6c
Ruslan Smagulov <r.smagulov@gmail.com>
2026-03-26 09:26:31 +0000

    upd

---
+++ 22/task2/build/CMakeFiles/task2_integration.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/r.smagulov/tasks/Parallelism/22/task2/src/task2_integration.cpp" "CMakeFiles/task2_integration.dir/src/task2_integration.cpp.o" "gcc" "CMakeFiles/task2_integration.dir/src/task2_integration.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 22/task2/build/CMakeFiles/task2_integration.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task2_integration.dir/src/task2_integration.cpp.o"
+  "CMakeFiles/task2_integration.dir/src/task2_integration.cpp.o.d"
+  "task2_integration"
+  "task2_integration.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task2_integration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
