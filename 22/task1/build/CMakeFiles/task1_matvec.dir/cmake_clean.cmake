bd98aa271f65bbfd898d9c2f059d0ca5d6e384f7
Ruslan Smagulov <r.smagulov@gmail.com>
2026-03-26 09:20:36 +0000

    upl

---
+++ 22/task1/build/CMakeFiles/task1_matvec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task1_matvec.dir/src/task1_matrix_vector.cpp.o"
+  "CMakeFiles/task1_matvec.dir/src/task1_matrix_vector.cpp.o.d"
+  "task1_matvec"
+  "task1_matvec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task1_matvec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
