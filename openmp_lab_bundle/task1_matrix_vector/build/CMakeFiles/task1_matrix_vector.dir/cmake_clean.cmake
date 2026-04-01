b54708a986c8c1368594b9182045f761e0108783
Ruslan Smagulov <r.smagulov@gmail.com>
2026-04-01 18:08:18 +0000

    task1

---
+++ openmp_lab_bundle/task1_matrix_vector/build/CMakeFiles/task1_matrix_vector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task1_matrix_vector.dir/src/main.cpp.o"
+  "CMakeFiles/task1_matrix_vector.dir/src/main.cpp.o.d"
+  "task1_matrix_vector"
+  "task1_matrix_vector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task1_matrix_vector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ openmp_lab_bundle/task1_matrix_vector/build/CMakeFiles/task1_matrix_vector.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for task1_matrix_vector.
+# This may be replaced when dependencies are built.
