8d2935b58ebc7c88c004d66144eec4993f501c7f
Ruslan Smagulov <r.smagulov@gmail.com>
2026-05-07 09:01:25 +0000

    upd

---
+++ 3/1/build/CMakeFiles/matrix_vector_benchmark.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrix_vector_benchmark.dir/src/matrix_vector_benchmark.cpp.o"
+  "CMakeFiles/matrix_vector_benchmark.dir/src/matrix_vector_benchmark.cpp.o.d"
+  "matrix_vector_benchmark"
+  "matrix_vector_benchmark.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matrix_vector_benchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
